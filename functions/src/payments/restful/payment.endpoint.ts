7f6f5af959b4aed30b9f65699472d765818a11a1
Matt DeKok <matt.dekok@gmail.com>
2021-03-24 09:08:16 -0500

    Add payment function and validation

--- functions/src/payments/restful/payment.endpoint.ts
+++ functions/src/payments/restful/payment.endpoint.ts
@@ -9,7 +9,7 @@ interface StripeKeys {
 }
 
 export default new Endpoint(
-    'donate',
+    'payment',
     RequestType.POST,
     async (req: Request, res: Response) => {
         const stripeKeys: StripeKeys = config().stripe;
@@ -23,17 +23,72 @@ export default new Endpoint(
         const amount: number = Math.abs(Math.round(parseFloat(req.body.amount) * 100));
         const tokenId: any = req.body.tokenId;
 
+        if (!name) {
+            return res.json({
+                body: req.body,
+                status: "error",
+                error: {
+                    type: "Missing Information",
+                    message: "Name is missing."
+                }
+            });
+        }
+
+        if (!email) {
+            return res.json({
+                body: req.body,
+                status: "failed",
+                error: {
+                    type: "Missing Information",
+                    message: "Email address is missing."
+                }
+            });
+        }
+
+        if (amount <= 1) {
+            return res.json({
+                body: req.body,
+                status: "failed",
+                error: {
+                    type: "Invalid Information",
+                    message: "Please enter an amount greater than 0."
+                }
+            });
+        }
+
+        if (!email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+            return res.json({
+                body: req.body,
+                status: "failed",
+                error: {
+                    type: "Invalid Information",
+                    message: "The email address you submitted is invalid."
+                }
+            });
+        }
+
+        if (!tokenId) {
+            return res.json({
+                body: req.body,
+                status: "failed",
+                error: {
+                    type: "Missing Information",
+                    message: "Payment token is missing."
+                }
+            });
+        }
+
         try {
             let charge = await stripe.charges.create({
                 amount: amount,
                 currency: "usd",
                 description: `Donation from ${name.length > 0 ? name : "anonymous"}`,
                 source: tokenId,
-                statement_descriptor: `Donation to mattdekok.dev`,
+                statement_descriptor: `Donation to Matt DeKok`,
                 receipt_email: email
             });
 
-            res.json({
+            return res.json({
                 body: req.body,
                 status: charge.status,
                 error:
@@ -45,10 +100,13 @@ export default new Endpoint(
                         : null
             });
         } catch (err) {
-            res.status(500).json({
+            return res.json({
                 body: req.body,
                 status: "failed",
-                error: err
+                error: {
+                    type: err.raw ? "ChargeFailed (" + err.raw.statusCode + ")" : null,
+                    message: err.raw ? err.raw.message : err.message
+                }
             });
         }
     }
