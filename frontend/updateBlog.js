667240f9d3dd796c34e7112553fe29a246729e15
Matt DeKok <matt.dekok@gmail.com>
2021-04-05 14:33:07 -0500

    Remove article markdown files from repo

--- frontend/updateBlog.js
+++ frontend/updateBlog.js
@@ -39,24 +39,36 @@ const storageDir = "blog/articles";
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
   });
 
-  const files = await admin.storage().bucket().getFiles({ directory: storageDir });
+  const files = await admin
+    .storage()
+    .bucket()
+    .getFiles({ directory: storageDir });
   const mdFiles = files[0].filter(file => file.name.includes(".md"));
   for (let i = 0; i < mdFiles.length; i++) {
     const file = mdFiles[i];
-    let fileContent = "";
-    file
-      .createReadStream()
-      .on("data", chunk => {
-        fileContent += chunk.toString();
-      })
-      .on("end", async () => {
+    new Promise(resolve => {
+      try {
         const filePath = file.name || "";
         const fileExtension = path.extname(filePath);
         const baseFileName = path.basename(filePath, fileExtension);
-        fs.appendFileSync(
-          `${articleDir}/${baseFileName}${fileExtension}`,
-          fileContent
-        );
-      });
+        const createPath = `${articleDir}/${baseFileName}${fileExtension}`;
+
+        let fileContent = "";
+        file
+          .createReadStream()
+          .on("data", chunk => {
+            fileContent += chunk.toString();
+          })
+          .on("end", async () => {
+            fs.appendFileSync(createPath, fileContent);
+            console.log(`Created: ${createPath}`);
+            resolve(true);
+          });
+      } catch (err) {
+        console.log("Error:", createPath);
+        console.log(err);
+        resolve(false);
+      }
+    });
   }
 })();
