3cb546c4ada57aa0588180bcf6fc75f9ce05c5b0
Matt DeKok <matt.dekok@gmail.com>
2021-04-08 07:49:09 -0500

    Update API Redirect

--- frontend/components/aux-functions.js
+++ frontend/components/aux-functions.js
@@ -7,18 +7,18 @@ export const formatDate = (date, time) => {
   };
   return time
     ? new Date(date).toLocaleString("en", {
-        ...options,
-        hour: "numeric",
-        minute: "numeric",
-        timeZoneName: "short"
-      })
+      ...options,
+      hour: "numeric",
+      minute: "numeric",
+      timeZoneName: "short"
+    })
     : new Date(date).toLocaleDateString("en", options);
 };
 
 export const apiRedirect = () => {
   if (!process.server) {
     if (window.location.host.includes("api.mattdekok.dev")) {
-      window.location.replace("https://www.mattdekok.dev");
+      window.location.replace(window.location.toString().replace('api.matt', 'www.matt'));
     }
   }
 }
