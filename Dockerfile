de736add1a872c95e56fb7035b9932bc8e6f170d
Matt Dekok <matt.dekok@gmail.com>
2022-03-16 10:02:13 -0500

    Docker Update

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,12 @@ FROM node:16
 
 WORKDIR /usr/mattdekok/app
 
+COPY ./frontend/package.json /usr/mattdekok/app/frontend/package.json
+COPY ./frontend/package-lock.json /usr/mattdekok/app/frontend/package-lock.json
+COPY ./package.json /usr/mattdekok/app/package.json
+
+RUN npm run build:docker
+
 COPY . .
 
-RUN npm run build:dev
-
-CMD [ "npm", "start" ]
+CMD [ "npm", "run", "start:docker" ]
--- frontend/pages/projects/index.vue
+++ frontend/pages/projects/index.vue
@@ -18,7 +18,7 @@
               image="me2"
               title="This Page"
               subtitle="Personal Website"
-              description="Firebase, Node.js, Vue, JavaScript, HTML, SCSS, Vuetify, Stripe"
+              description="Firebase, Node.js, Vue, JavaScript, HTML, SCSS, Vuetify, Docker Compose, Github Actions"
             />
           </v-col>
           <v-col cols="12" md="6" lg="4" class="project">
